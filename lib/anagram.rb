e3889af2a90f801d152ac564295ff2223d7a0dfb
Janice <janiceralecha@gmail.com>
2022-11-02 20:18:01 -0500

    commit tree 19b122439cf9008ea73fc2265b00018334aca3cd

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+    
+    def initialize(word)
+        @word = word
+    end
+
+    def match(anagram)
+        splitWord = @word.split("").sort().join()
+        splitAnagram = anagram.map {|word| word.split("")}
+        sortAnagram = splitAnagram.map{|word| word.sort().join()}
+
+        anagram.filter do |a|
+            a.split("").sort() == @word.chars.sort()
+            end
+    
+        end
+      
+    end
